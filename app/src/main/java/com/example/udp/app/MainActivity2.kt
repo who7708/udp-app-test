1ac0ed2b30079e73dc75fd51bab1e3f6ffa9a68e
Chris <who7708@163.com>
2024-05-29 01:00:37 +0800

    fragment+viewmodel

---
+++ app/src/main/java/com/example/udp/app/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.udp.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.udp.app.ui.main.MainFragment
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
